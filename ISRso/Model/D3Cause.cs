15e3b7d6325b662c969bfdfa757334451120ed8f
Andrey Podzorov <aterrion@gmail.com>
2022-01-09 23:20:19 +0200

    add few class and view

---
+++ ISRso/Model/D3Cause.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ISRso.Model
+{
+    class D3Cause : INotifyPropertyChanged
+    {
+        #region Private
+        private int id;
+        private string causeNum ;
+        private string procNum;
+        private string causeCore;
+        private int d3ID;
+        private int d3DBID;
+        private int erdrId;
+        private int judjeId;
+        private bool isDeleted;
+
+        #endregion
+
+        #region Propertys
+        public int Id
+        {
+            get { return id; }
+            set {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string CauseNum 
+        {
+            get { return causeNum; }
+            set {
+                causeNum = value;
+                OnPropertyChanged("CauseNum");
+            }
+        }
+        public string ProcNum
+        {
+            get { return procNum; }
+            set {
+                procNum = value;
+                OnPropertyChanged("ProcNum");
+            }
+        }
+        public string CauseCore
+        {
+            get { return causeCore; }
+            set {
+                causeCore = value;
+                OnPropertyChanged("CauseCore");
+            }
+        }
+        public int D3ID
+        {
+            get { return d3ID; }
+            set {
+                d3ID = value;
+                OnPropertyChanged("D3ID");
+            }
+        }
+        public int D3DBID
+        {
+            get { return d3DBID; }
+            set {
+                d3DBID = value;
+                OnPropertyChanged("D3DBID");
+            }
+        }
+        public int ErdrId
+        {
+            get { return erdrId; }
+            set {
+                erdrId = value;
+                OnPropertyChanged("ErdrId");
+            }
+        }
+        public int JudjeId
+        {
+            get { return judjeId; }
+            set {
+                judjeId = value;
+                OnPropertyChanged("JudjeId");
+            }
+        }
+        public bool IsDeleted
+        {
+            get { return isDeleted; }
+            set {
+                isDeleted = value;
+                OnPropertyChanged("IsDeleted");
+            }
+        }
+
+        #endregion
+
+
+        public D3Cause()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ISRso/Model/ErdrNum.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ISRso.Model 
+{
+    class ErdrNum : INotifyPropertyChanged
+    {
+        #region Private
+        private int id;
+        private string number;
+        private DateTime createDate;
+        private int createUser;
+        private DateTime editDate;
+        private int editUser;
+        private int version;
+        private bool isDeleted;
+
+        #endregion
+        
+        #region Propertys
+        public int Id
+        {
+            get { return id; }
+            set {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string Number
+        {
+            get { return number; }
+            set {
+                number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+        public string _code { get; private set; }
+        public string _year { get; private set; }
+        public string _orgcode { get; private set; }
+        public string _org { get; private set; }
+        public string _num { get; private set; }
+        public DateTime CreateDate
+        {
+            get { return createDate; }
+            set {
+                createDate = value;
+                OnPropertyChanged("Createdate");
+            }
+        }
+        public int CreateUser
+        {
+            get { return createUser; }
+            set {
+                createUser = value;
+                OnPropertyChanged("CreateUser");
+            }
+        }
+        public DateTime EditDate
+        {
+            get { return editDate; }
+            set {
+                editDate = value;
+                OnPropertyChanged("EditDate");
+            }
+        }
+        public int EditUser
+        {
+            get { return editUser; }
+            set {
+                editUser = value;
+                OnPropertyChanged("EditUser");
+            }
+        }
+        public int Version
+        {
+            get { return version; }
+            set {
+                version = value;
+                OnPropertyChanged("Version");
+            }
+        }
+        public bool IsDeleted
+        {
+            get { return isDeleted; }
+            set {
+                isDeleted = value;
+                OnPropertyChanged("IsDeleted");
+            }
+        }
+
+        #endregion
+
+
+        public ErdrNum()
+        {
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
